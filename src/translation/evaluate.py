16900226e0489286c4743dd5d1eb61ca39d02239
Ilyass-42 <ilyass.elassad.contact@gmail.com>
2026-04-25 12:05:32 +0200

    add evaluate.py : BLEU evaluation script used to score fine -tuned model (18.30, max_ngram_order=3)

--- src/translation/evaluate.py
+++ src/translation/evaluate.py
@@ -1,22 +1,32 @@
 from sacrebleu.metrics import BLEU
-from translate import translate
 import pandas as pd
 from pathlib import Path
+from transformers import MarianTokenizer, MarianMTModel
 
-test_path = Path(__file__).parent.parent.parent / "data" / "Test.csv"
+test_path = "/content/lost-in-darija/data/Test.csv"
+tokenizer = MarianTokenizer.from_pretrained("/content/lost-in-darija/models/fine_tuned_marian")
+model = MarianMTModel.from_pretrained("/content/lost-in-darija/models/fine_tuned_marian")
+model = model.to("cuda")
+
+def translate(phrases: list[str]) :
+    phrases = [">>ary<< " + texte for texte in phrases]
+    inputs = tokenizer(phrases, return_tensors="pt",padding=True,truncation= True)
+    inputs = {k: v.to("cuda") for k, v in inputs.items()}
+    translated = model.generate(**inputs)
+    res = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+
+    return res
+
+batch_size = 32
+test_df = pd.read_csv(test_path)
+hypohteses = []
+references = []
+for index in range(0,len(test_df["eng"]),batch_size):
+    predicted_phrase = translate(test_df["eng"][index:index+batch_size])
+    hypohteses.extend(predicted_phrase)
+references = [list(test_df["darija_ar"])]
 
 
-if __name__=="__main__":
-    test_df = pd.read_csv(test_path)
-    hypohteses = []
-    references = []
-    for phrase_eng in test_df.head(10)["eng"]:
-        predicted_phrase = translate(phrase_eng)
-        hypohteses.append(predicted_phrase)
-    for phrases_darija in test_df.head(10)["darija_ar"]:
-        references.append([phrases_darija])
-
-
-    bleu = BLEU()
-    bleu_score = bleu.corpus_score(hypohteses,references)
-    print(f"\n",{bleu_score})
+bleu = BLEU(max_ngram_order=3)
+bleu_score = bleu.corpus_score(hypohteses,references)
+print(f"\n",{bleu_score})
