8684484cb61f614f028a93c9a0d255e0439bb96a
Ilyass-42 <ilyass.elassad.contact@gmail.com>
2026-05-01 00:30:36 +0200

    feat: add beam search num_beams=4 length_penalty=1.2

--- src/translation/evaluate.py
+++ src/translation/evaluate.py
@@ -5,7 +5,7 @@ from transformers import MarianTokenizer, MarianMTModel
 
 
 test_path = Path(__file__).parent.parent.parent / "data" / "Test.csv"
-model_path = Path(__file__).parent.parent.parent / "models" / "fine_tuned_marian_v2"
+model_path = Path(__file__).parent.parent.parent / "models" / "fine_tuned_marian"
 tokenizer = MarianTokenizer.from_pretrained(model_path)
 model = MarianMTModel.from_pretrained(model_path)
 model = model.to("cuda")
@@ -14,7 +14,7 @@ def translate(phrases: list[str]) :
     phrases = [">>ary<< " + texte for texte in phrases]
     inputs = tokenizer(phrases, return_tensors="pt",padding=True,truncation= True)
     inputs = {k: v.to("cuda") for k, v in inputs.items()}
-    translated = model.generate(**inputs)
+    translated = model.generate(**inputs,num_beams=4,length_penalty=1.2)
     res = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
     return res
