093a0eb60c7193f91c61bc22820dfa6c6f0572ee
Ilyass-42 <ilyass.elassad.contact@gmail.com>
2026-05-01 23:35:42 +0200

    v2 dataset: add comments + web_data + fix prepare_data.py

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -19,22 +19,26 @@ def load_and_prepare(subset_name):
     df = df[df["darija_ar"].apply(is_arabic)]
     return df
 
-train_path = Path(__file__).parent/"Train.csv" #data/Train.csv
-val_path = Path(__file__).parent/"Val.csv"
+train_path = Path(__file__).parent/"Train_v5_backup.csv" #data/Train.csv
+val_path = Path(__file__).parent/"Val_v5_backup.csv"
 
 df_comments = load_and_prepare("comments")
 df_web_data = load_and_prepare("web_data")
 df_train = pd.read_csv(train_path)
+df_val = pd.read_csv(val_path)
 
-df_merged = pd.concat([df_train,df_comments,df_web_data],ignore_index=True)
+df_merged = pd.concat([df_train,df_comments,df_web_data,df_val],ignore_index=True)
 
 df_merged.drop_duplicates(subset=["eng","darija_ar"],inplace=True)
 
 df_new_train,df_new_val = train_test_split(df_merged,test_size=0.10,random_state=42)
 
 
+output_train = Path(__file__).parent/"Train.csv"
+output_val = Path(__file__).parent/"Val.csv"
 
-df_new_train.to_csv(train_path,index = False)
-df_new_val.to_csv(val_path,index = False)
+
+df_new_train.to_csv(output_train,index = False)
+df_new_val.to_csv(output_val,index = False)
 
 
