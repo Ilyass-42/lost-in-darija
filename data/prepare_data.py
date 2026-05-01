a7593619fbc4b23614cd7afdf4719524f0a93e2e
Ilyass-42 <ilyass.elassad.contact@gmail.com>
2026-05-01 23:19:41 +0200

    add prepare_data.py

---
+++ data/prepare_data.py
@@ -0,0 +1,40 @@
+import re
+from datasets import load_dataset
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+def is_arabic(text):
+    if not text or len(text)==0:
+        return False
+    arabic_chars = len(re.findall(r'[\u0600-\u06FF]', text))
+    return arabic_chars/len(text) > 0.5
+
+
+def load_and_prepare(subset_name):
+    dataset = load_dataset("atlasia/darija_english", subset_name)
+    df = dataset["train"].to_pandas()
+    df.rename(columns={"darija":"darija_ar","english":"eng"},inplace=True)
+    df = df.drop(["id","source"],axis=1)
+    df = df[df["darija_ar"].apply(is_arabic)]
+    return df
+
+train_path = Path(__file__).parent/"Train.csv" #data/Train.csv
+val_path = Path(__file__).parent/"Val.csv"
+
+df_comments = load_and_prepare("comments")
+df_web_data = load_and_prepare("web_data")
+df_train = pd.read_csv(train_path)
+
+df_merged = pd.concat([df_train,df_comments,df_web_data],ignore_index=True)
+
+df_merged.drop_duplicates(subset=["eng","darija_ar"],inplace=True)
+
+df_new_train,df_new_val = train_test_split(df_merged,test_size=0.10,random_state=42)
+
+
+
+df_new_train.to_csv(train_path,index = False)
+df_new_val.to_csv(val_path,index = False)
+
+
